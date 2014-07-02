5e2661b42e5aa0bee3a7a3c1cf4fab7849138b7e
Diego Evaldt Silveira <diegoesilveira@gmail.com>
2014-07-02 13:29:04 -0300

    T4 - Tratamento de arquivos
    Segue trabalho não concluido de tratamento de arquivos

---
+++ T4 - Tratando arquivos/src/com/senac/registrobanco/view/BancoView.java
@@ -0,0 +1,18 @@
+package com.senac.registrobanco.view;
+
+import com.senac.bank.console.BankConsole;
+
+public class BancoView extends BankConsole{
+
+	public void printDividendsOperation()
+	{
+		System.out.println( "3 - Dividendos" );
+	}
+	
+	public void printMenu()
+	{
+		System.out.println( " -----MENU----- "
+				+ "\n1 - Deposito"
+				+ "\n2 - Retirar" );
+	}
+}
