2b142878564043478c89fdaa4c39919a9b96c5d7
Diego Evaldt Silveira <diegoesilveira@gmail.com>
2014-07-10 20:54:08 -0300

    T1 - Listas Encadeadas
    Criação dos metodos da classe AgendaListaEncadeada.

--- T1 - Listas Encadeadas/src/com/senac/agenda/estruturas/AgendaListaEncadeada.java
+++ T1 - Listas Encadeadas/src/com/senac/agenda/estruturas/AgendaListaEncadeada.java
@@ -1,5 +1,25 @@
 package com.senac.agenda.estruturas;
 
-public class AgendaListaEncadeada {
+import com.senac.estruturas.Nodo;
+
+public class AgendaListaEncadeada <T>{
+	
+	protected Nodo<T> head;
+	protected Nodo<T> tail;
+	
+	public AgendaListaEncadeada() {
+		head = null;
+		tail = null;
+	}
+	
+	public void inserir(Nodo<T> novo){
+		
+		novo.setAvanca(head);
+		if(head!= null)
+			head.setPrevious(novo);
+		head=novo;
+		
+	}
+	
 
 }
