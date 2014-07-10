c483750b4180708af510018e8e694b2839eddacc
Diego Evaldt Silveira <diegoesilveira@gmail.com>
2014-07-10 20:43:49 -0300

    T1 - Listas Encadeadas
    Criado os metodos da classe Nodo.

--- T1 - Listas Encadeadas/src/com/senac/agenda/estruturas/Nodo.java
+++ T1 - Listas Encadeadas/src/com/senac/agenda/estruturas/Nodo.java
@@ -1,5 +1,48 @@
 package com.senac.agenda.estruturas;
 
-public class Nodo {
+	
+	public class Nodo<T> {
+		private T data;
+		private Nodo<T> avanca;
+		private Nodo<T> anterior;
+		
+		public Nodo()   //construtor setado valores nulos para as variaveis
+		{
+			data = null;
+			avanca = null;
+		}
 
-}
+		public Nodo(T chave) {
+			this.data = chave;
+			this.avanca = null;
+		}
+
+		public T getData() {
+			return data;
+		}
+
+		public void setData(T data) {
+			this.data = data;
+		}
+
+		public Nodo<T> getAvanca() {
+			return avanca;
+		}
+
+		public void setAvanca(Nodo<T> avanca) {
+			this.avanca = avanca;
+		}
+
+		public Nodo<T> getPrevious() {
+			return anterior;
+		}
+
+		public void setPrevious(Nodo<T> ant) {
+			this.anterior = ant;
+		}
+		
+	}
+	
+	
+
+
