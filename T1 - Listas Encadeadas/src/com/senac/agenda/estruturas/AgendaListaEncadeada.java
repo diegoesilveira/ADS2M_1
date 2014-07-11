782f43ed395f29c8514a6af14148fb79fab53c13
Diego Evaldt Silveira <diegoesilveira@gmail.com>
2014-07-11 06:00:15 -0300

    T1 - Listas Encadeadas
    Criado os metodos da classe AgendaListaEncadeada.

--- T1 - Listas Encadeadas/src/com/senac/agenda/estruturas/AgendaListaEncadeada.java
+++ T1 - Listas Encadeadas/src/com/senac/agenda/estruturas/AgendaListaEncadeada.java
@@ -1,5 +1,6 @@
 package com.senac.agenda.estruturas;
 
+
 public class AgendaListaEncadeada <T>{
 	
 	protected Nodo<T> head;
@@ -20,6 +21,57 @@ public class AgendaListaEncadeada <T>{
 			tail = novo;
 		
 	}
+	
+	public void inserir(Nodo<T> novo, Nodo<T> anterior)
+	{
+		if (anterior == null) {
+			novo.setAvanca(head);
+			head = novo;
+			if (tail == null)
+				tail = head;
+		} else {
+			novo.setAvanca(anterior.getAvanca());
+			novo.setPrevious(anterior);
+			anterior.setAvanca(novo);
+			if (anterior == tail)
+				tail = novo;
+		}
+	}
+	
+	public void append(Nodo<T> novo)
+	{
+		if (tail != null) {
+			tail.setAvanca(novo);
+			novo.setPrevious(tail);
+		} else {
+			head = novo;
+		}
+		tail = novo;
+	}
+	
+	public Nodo<T> getTail()
+	{
+		return tail;
+	}
+	
+	public Nodo<T> getHead()
+	{
+		return head;
+	}
+
+	public void remove(Nodo<T> nodo) {
+		Nodo<T> anterior = nodo.getPrevious();
+		Nodo<T> avanca = nodo.getAvanca();
+		if (anterior != null)
+			anterior.setAvanca(avanca);
+		else
+			head = avanca;
+		if (avanca != null)
+			avanca.setPrevious(anterior);
+		else
+			tail = anterior;
+	}
+
 	
 
 }
